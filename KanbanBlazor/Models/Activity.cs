9252e4e259ab8f4967854c991b4600a09429f173
gui-marchioro <gui02_marchioro@hotmail.com>
2024-05-05 18:31:31 -0300

    Added Activity data validation.

--- KanbanBlazor/Models/Activity.cs
+++ KanbanBlazor/Models/Activity.cs
@@ -1,13 +1,26 @@
 ﻿using KanbanBlazor.Models.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KanbanBlazor.Models;
 
 public class Activity
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Title required")]
+    [StringLength(50, ErrorMessage = "Maximum length is 50 characters")]
     public string? Title { get; set; }
+
+    [StringLength(200, ErrorMessage = "Maximum length is 200 characters")]
     public string? Description { get; set; }
+
+    [Required(ErrorMessage = "Status required")]
     public ActivityStatus Status { get; set; }
-    public int ListGroupId { get; set; }
+
+    [Required(ErrorMessage = "Group required")]
+    [ForeignKey(nameof(ListGroup))]
+    public int? ListGroupId { get; set; }
     public virtual ListGroup? ListGroup { get; set; }
 }
