24b81bcf15ebba71e71d377fc0bd4e08c53f9e5c
gui-marchioro <gui02_marchioro@hotmail.com>
2024-05-05 23:09:37 -0300

    Added List Group CRUD.

--- KanbanBlazor/Data/Services/ListGroupsService.cs
+++ KanbanBlazor/Data/Services/ListGroupsService.cs
@@ -20,4 +20,39 @@ public class ListGroupsService
             return await context.Groups.ToListAsync();
         }
     }
+
+    public async Task<ListGroup> Add(ListGroup listGroup)
+    {
+        using (var context = this._dbContextFactory.CreateDbContext())
+        {
+            context.Groups.Add(listGroup);
+            await context.SaveChangesAsync();
+        }
+
+        return listGroup;
+    }
+
+    public async Task<ListGroup> Delete(int id)
+    {
+        ListGroup? listGroup;
+        using (var context = this._dbContextFactory.CreateDbContext())
+        {
+            listGroup = await context.Groups.FindAsync(id);
+            context.Groups.Remove(listGroup);
+            await context.SaveChangesAsync();
+        }
+
+        return listGroup;
+    }
+
+    public async Task<ListGroup> Update(ListGroup listGroup)
+    {
+        using (var context = this._dbContextFactory.CreateDbContext())
+        {
+            context.Entry(listGroup).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+
+        return listGroup;
+    }
 }
--- KanbanBlazor/Models/ListGroup.cs
+++ KanbanBlazor/Models/ListGroup.cs
@@ -1,8 +1,12 @@
-﻿namespace KanbanBlazor.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KanbanBlazor.Models;
 
 public class ListGroup
 {
+    [Key]
     public int Id { get; set; }
+    [Required(ErrorMessage = "A name is required")]
     public string? Name { get; set; }
     public List<Activity>? Tasks { get; set; }
 }
